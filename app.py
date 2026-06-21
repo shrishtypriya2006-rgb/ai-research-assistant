95ab979e14d2299f811246008f46449f605ee73c
Shrishty Priya <shrishtypriya2006@gmail.com>
2026-06-21 18:02:19 +0530

    Updated app with Streamlit secrets

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 import streamlit as st
 from google import genai
 
-API_KEY = "AQ.Ab8RN6LVKt_2odP9cNmMwq3D7deEX-GjJcAD4h8lzNwKGSuX1A"
-
+# Create Gemini client using Streamlit Secrets
 client = genai.Client(api_key=st.secrets["API_KEY"])
 
+# App title
 st.title("🧠 AI Research Assistant")
 
+# User input
 topic = st.text_input("Enter a topic")
 
+# Button
 if st.button("Research"):
+
     if topic:
+
         try:
             response = client.models.generate_content(
                 model="gemini-2.5-flash",
                 contents=f"Explain {topic} in simple terms with examples."
             )
 
+            st.subheader("Research Result")
             st.write(response.text)
 
         except Exception as e:
             st.error(f"Error: {e}")
 
     else:
-        st.warning("Please enter a topic")
+        st.warning("Please enter a topic.")
